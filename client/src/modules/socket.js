ce70b18efde5e0356fd73a0ea16b2c80f12ec523
timcsy <timcsy@timcsyde-MacBook-Pro.local>
2019-08-30 03:50:28 +0800

    finish message driven websocket machenism

--- client/src/modules/socket.js
+++ client/src/modules/socket.js
@@ -14,30 +14,6 @@ class MainSocket {
 			const msg = JSON.parse(message)
 			// console.log(msg)
 			this.callbacks[msg.on].forEach(async(callback) => await callback(msg))
-
-			if (msg.cmd === 'after:render') {
-				afterRender(msg)
-			} else if (msg.cmd === 'ready') {
-				await prepareSendOffer()
-			} else if (msg.type === 'offer') {
-				receiveOffer(new RTCSessionDescription(msg.sdp), msg.sendFrom)
-			} else if (msg.type === 'answer') {
-				receiveAnswer(new RTCSessionDescription(msg.sdp))
-			} else if (msg.type === 'ice') {
-				receiveICE(new RTCIceCandidate(msg.ice))
-			} else if (msg.type === 'disconnected') {
-				isServerMode = true
-			} else if (msg.cmd === 'speech:start') {
-				await call(true, msg.sendFrom)
-			} else if (msg.cmd === 'speech:data') {
-				play(btof(msg.data))
-			} else if (msg.cmd === 'speech:local') {
-				showLocalSpeech(msg)
-			} else if (msg.cmd === 'speech:remote') {
-				showRemoteSpeech(msg)
-			} else if (msg.cmd === 'speech:stop') {
-				await hangup(true)
-			}
 		}
 
 		this.ws.onopen = () => {
